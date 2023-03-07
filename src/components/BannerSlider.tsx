0fd95e35f0df7c3785ca24a9edf09edb7c187e83
kamalesh maity <kamalesh.maity@codeclouds.in>
2023-03-07 12:29:42 +0530

    image lint issue fixed

--- src/components/BannerSlider.tsx
+++ src/components/BannerSlider.tsx
@@ -7,10 +7,18 @@ import 'swiper/css/navigation'
 const BannerSlider = (): JSX.Element => (
   <section className="offer-slider">
     <Swiper navigation={true} modules={[Navigation]}>
-      <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-7.jpg" /></SwiperSlide>
-      <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-8.jpg" /></SwiperSlide>
-      <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-9.jpg" /></SwiperSlide>
-      <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-6.jpg" /></SwiperSlide>
+      <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+      </SwiperSlide>
     </Swiper>
   </section>
 )
