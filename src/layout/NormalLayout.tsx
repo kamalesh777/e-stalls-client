3bbd3e3e8099aaf84c455dec3719beca50d0df6c
kamalesh777 <ksmaity777@gmail.com>
2023-03-07 19:55:07 +0530

    footer added

--- src/layout/NormalLayout.tsx
+++ src/layout/NormalLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '@/common/Footer'
 import HeaderComponent from '@/common/Header'
 
 const NormalLayout = ({ children }): JSX.Element => (
   <>
     <HeaderComponent />
     <div className="content">{children}</div>
+    <Footer />
   </>
 )
 export default NormalLayout
