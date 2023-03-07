e9bb4af4531ba43565b40fadb6071641d3ed077e
kamalesh maity <kamalesh.maity@codeclouds.in>
2023-03-07 12:35:05 +0530

    autoplay added for bannerSlider

--- src/components/BannerSlider.tsx
+++ src/components/BannerSlider.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { Navigation } from 'swiper'
+import { Autoplay, Navigation } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 
 const BannerSlider = (): JSX.Element => (
   <section className="offer-slider">
-    <Swiper navigation={true} modules={[Navigation]}>
+    <Swiper
+      navigation={true}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay, Navigation]}
+    >
       <SwiperSlide>
         <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
       </SwiperSlide>
