588cc8c8fdde6f3fe83e9ea280fd4e4288f828aa
kamalesh maity <kamalesh.maity@codeclouds.in>
2023-03-09 11:38:47 +0530

    redirect if anyone hit profile

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/profile',
+        destination: '/profile/orders',
+        permanent: true,
+      },
+    ]
+  },
 }
 module.exports = nextConfig
