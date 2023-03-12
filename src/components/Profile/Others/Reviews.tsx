9a9021e76b309768a4b6247b4cabf9197fb4e4e0
Kamalesh Maity <ksmaity777@gmail.com>
2023-03-12 17:32:46 +0530

    review list page ready

--- src/components/Profile/Others/Reviews.tsx
+++ src/components/Profile/Others/Reviews.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
-import { Row, Col, Card } from 'react-bootstrap'
-import { BiPlus } from 'react-icons/bi'
-import { HiOutlineOfficeBuilding, HiOutlineHome } from 'react-icons/hi'
+import { Col, Badge, Dropdown } from 'react-bootstrap'
+import { BiCheckSquare, BiDotsVerticalRounded, BiEdit, BiPlus, BiStar, BiTrash } from 'react-icons/bi'
 import ExtraCardHeader from '@/common/ExtraCardHeader'
 import ReviewRating from '@/common/Modal/ReviewRating'
 
-import reviewData from "@/mock/reviews.json"
+import reviewData from '@/mock/reviews.json'
 
 interface reviewTypes {
   _id: string
@@ -20,7 +19,31 @@ interface reviewTypes {
 
 const Reviews = (): JSX.Element => {
   const [openModal, setOpenModal] = useState<boolean>(false)
-  const [reviewList, setReviewList] = useState<reviewTypes[]>([])
+  const [reviewList, setReviewList] = useState<reviewTypes[]>(reviewData || [])
+
+  const editHandler = (id: string): void => {
+    console.log(id)
+  }
+
+  // dropdown option
+  const individualDropdown = (obj: reviewTypes): JSX.Element => (
+    <Dropdown className="text-center">
+      <Dropdown.Toggle id="dropdown-basic" className="simple-dropdown">
+        <BiDotsVerticalRounded />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1" onClick={() => editHandler(obj._id)}>
+          <BiEdit className="me-2 fs-5" />
+          Edit
+        </Dropdown.Item>
+        <Dropdown.Item className="text-danger">
+          <BiTrash className="me-2 fs-5" />
+          Delete
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  )
   return (
     <div className="content-wrapper">
       <ExtraCardHeader
@@ -32,27 +55,49 @@ const Reviews = (): JSX.Element => {
           </button>
         }
       />
-      <Row>
-        {reviewList.map(obj => (
-          <Col key={obj._id} md={6}>
-            <Card className="adress-box">
-              <Card.Body>
-                <div className={`addres-icon ${obj.default ? 'active' : ''}`}>
-                  {obj.address_type === 'office' ? (
-                    <HiOutlineOfficeBuilding className="fs-3" />
-                  ) : (
-                    <HiOutlineHome className="fs-3" />
-                  )}
+      <div className="profile-review">
+        <div className="review-list-card">
+          <div className="review-haeder">
+            <Col sm={5} md={6}>
+              Product Name
+            </Col>
+            <Col sm={2} md={1}>
+              Rating
+            </Col>
+            <Col sm={4} md={4}>
+              Review
+            </Col>
+            <Col sm={1} md={1}>
+              Action
+            </Col>
+          </div>
+          {reviewList.map(obj => (
+            <div className="review-rows" key={obj._id}>
+              <Col sm={5} md={6}>
+                <div className="d-flex align-items-center">
+                  <div className="review-product mb-0">
+                    <img src={obj.product_image} className="img-fluid" />
+                  </div>
+                  <h2 className="fs-6 mb-0">Nova NHT 1039 USB Trim....</h2>
                 </div>
+              </Col>
+              <Col sm={2} md={1}>
+                <Badge bg="success" className="fs-7">
+                  {obj.rating}
+                  <BiStar className="ms-1" />
+                </Badge>
+              </Col>
+              <Col sm={4} md={4}>
+                {obj.review_description}
+              </Col>
 
-                <h4 className="fs-6">{obj.name}</h4>
-                <p className="fs-7 mb-0">{obj.address}</p>
+              <Col sm={1} md={1}>
                 {individualDropdown(obj)}
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+              </Col>
+            </div>
+          ))}
+        </div>
+      </div>
       <ReviewRating {...{ openModal, setOpenModal }} />
     </div>
   )
